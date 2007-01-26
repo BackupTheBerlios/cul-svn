d9acfd0b9923706720f9b70412c50d32f2f84d9d
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-01-26 22:03:33 +0000

    add module with basic support for file operations

---
+++ trunk/include/cul/cul_file.h
@@ -0,0 +1,23 @@
+#if !defined(CUL_FILE_H)
+#define CUL_FILE_H
+
+#include <cul/cul_global.h>
+#include <cul/cul_string_raw.h>
+
+#include <stdio.h>
+
+static inline FILE *cul_fopen(const char *filename, const char *mode)           { return fopen(filename, mode); }
+static inline int cul_fflush(FILE *stream)                                      { return fflush(stream); }
+static inline int cul_fclose(FILE *stream)                                      { return fclose(stream); }
+
+static inline int cul_feof(FILE *stream)                                        { return feof(stream); }
+static inline char *cul_fgets(char *str, int size, FILE *stream)                { return fgets(str, size, stream); }
+
+cul_bool cul_file_readable(const char *filename);
+cul_bool cul_file_writeable(const char *filename);
+
+size_t cul_file_lines(const char *filename);
+cul_errno cul_file_read_lines(const char *filename, size_t size, char ***contents, size_t *lines);
+cul_errno cul_file_write_lines(const char *filename, char **contents);
+
+#endif
---
+++ trunk/src/cul_file.c
@@ -0,0 +1,110 @@
+#include <cul/cul_file.h>
+#include <cul/cul_list.h>
+
+cul_bool cul_file_readable(const char *filename) {
+	FILE *fid;
+
+	if( filename == NULL || (fid = cul_fopen(filename, "r")) == NULL )
+		return CUL_FALSE;
+	cul_fclose(fid);
+	return CUL_TRUE;
+}
+
+cul_bool cul_file_writeable(const char *filename) {
+	FILE *fid;
+
+	if( filename == NULL || (fid = cul_fopen(filename, "w")) == NULL )
+		return CUL_FALSE;
+	cul_fclose(fid);
+	return CUL_TRUE;
+}
+
+size_t cul_file_lines(const char *filename) {
+	CUL_UNUSED(filename);
+	CUL_ERROR_ERRNO_RET_VAL(0, CUL_ESTUB);
+}
+
+cul_errno cul_file_read_lines(const char *filename, size_t size, char ***contents, size_t *lines) {
+	if( filename == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(CUL_EINVAL, CUL_EINVAL);
+
+	FILE *stream = cul_fopen(filename, "r");
+	if( stream == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(CUL_EFACCESS, CUL_EFACCESS);
+
+	/* initialize list */
+	CulList *list = NULL;
+
+	/* initialize output data */
+	*contents = NULL;
+	*lines = 0;
+
+	/* read file */
+	size_t list_size = 0;
+	while( !cul_feof(stream) ) {
+		char buffer[size];
+
+		if( !cul_fgets(buffer, size, stream) ) {
+			if( cul_feof(stream) )
+				break;
+			CUL_ERROR_ERRNO_RET_VAL(CUL_EFIO, CUL_EFIO);
+		}
+
+		size_t buffer_size = cul_strlen(buffer);
+
+		/* strip new line character at the end */
+		if( buffer[buffer_size-1] == CUL_STR_NEWLINE )
+			cul_strnull(buffer + (--buffer_size));
+
+		CulList *node = cul_list_new(cul_strdup_size(buffer, buffer_size));
+		if( node == NULL ) {
+			/* clean */
+			cul_list_free_all(cul_list_first(list), cul_free);
+			cul_fclose(stream);
+			CUL_ERROR_ERRNO_RET_VAL(CUL_ENOMEM, CUL_ENOMEM);
+		}
+
+		/* insert list node */
+		list = cul_list_insert_next_node(list, node);
+		list_size += 1;
+	}
+
+	/* back to front of the list */
+	list = cul_list_first(list);
+
+	char **linesv = cul_malloc( (list_size+1)*sizeof(char *) );
+	if( linesv == NULL ) {
+		/* clean */
+		cul_list_free_all(list, cul_free);
+		cul_fclose(stream);
+		CUL_ERROR_ERRNO_RET_VAL(CUL_ENOMEM, CUL_ENOMEM);
+	}
+
+	/* set output variables */
+	*contents = linesv;
+	*lines = list_size;
+
+	/* copy data */
+	linesv[list_size] = NULL;
+	for( CulList *node = list; list_size--; node = cul_list_next(node) )
+		linesv[list_size] = list->data;
+
+	/* remove temporary data */
+	cul_list_free_all(list, NULL);
+	cul_fclose(stream);
+
+	return CUL_SUCCESS;
+}
+
+cul_errno cul_file_write_lines(const char *filename, char **contents) {
+	CUL_UNUSED(contents);
+	if( filename == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(CUL_EINVAL, CUL_EINVAL);
+
+	FILE *fid = cul_fopen(filename, "w");
+	if( fid == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(CUL_EFACCESS, CUL_EFACCESS);
+	cul_fclose(fid);
+
+	CUL_ERROR_ERRNO_RET_VAL(CUL_ESTUB, CUL_ESTUB);
+}
