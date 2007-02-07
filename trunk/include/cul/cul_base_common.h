5c3965a8fdb18e1edd9d843dee16400359327906
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-02-07 06:31:50 +0000

    remove weird function

--- trunk/include/cul/cul_base_common.h
+++ trunk/include/cul/cul_base_common.h
@@ -2,11 +2,8 @@
 #define CUL_BASE_COMMON_H
 
 #include <cul/cul_global_base.h>
-#include <stdio.h>
 
 static inline size_t cul_matrix_pos(size_t x, size_t y, size_t width);
-
-int cul_fscanf_skip_line(FILE *stream);
 
 /* implementations */
 
