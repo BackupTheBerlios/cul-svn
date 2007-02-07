dd35e01594b1e3095699f63f64efe614a8293a04
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-02-07 05:37:53 +0000

    * implement whole tree free function  * traverse functions  * insert functions

--- trunk/include/cul/cul_tree.h
+++ trunk/include/cul/cul_tree.h
@@ -26,23 +26,56 @@ CulTree *cul_tree_init(CulTree *t);
 void cul_tree_free(CulTree *t, cul_free_f *free_item);
 void cul_tree_free_all(CulTree *t, cul_free_f *free_item);
 
+/* Basic Tree Operators */
+
+static inline CulTree *cul_tree_parent(CulTree *t);
+static inline CulTree *cul_tree_child(CulTree *t);
+static inline CulTree *cul_tree_next(CulTree *t);
+static inline CulTree *cul_tree_prev(CulTree *t);
+CulTree *cul_tree_first(CulTree *t);
+CulTree *cul_tree_last(CulTree *t);
+CulTree *cul_tree_root(CulTree *t);
+
+/* Resize */
+
+CulTree *cul_tree_insert_next(CulTree *t, cul_ptr data);
+CulTree *cul_tree_insert_prev(CulTree *t, cul_ptr data);
+CulTree *cul_tree_insert_child_first(CulTree *t, cul_ptr data);
+CulTree *cul_tree_insert_child_last(CulTree *t, cul_ptr data);
+
 /* implementations */
 
 static inline CulTree *cul_tree_new_struct(void) {
 	return cul_slab_new(sizeof(CulTree));
 }
 
-static inline void cul_tree_free_struct(CulTree *const t) {
+static inline void cul_tree_free_struct(CulTree *t) {
 	cul_slab_free(sizeof(CulTree), t);
 }
 
-static inline CulTree *cul_tree_init_struct(CulTree *const t, CulTree *parent, CulTree *next, CulTree *prev, CulTree *child, cul_ptr data) {
+static inline CulTree *cul_tree_init_struct(CulTree *t, CulTree *parent, CulTree *next, CulTree *prev, CulTree *child, cul_ptr data) {
 	t->data = data;
 	t->parent = parent;
 	t->next = next;
 	t->prev = prev;
 	t->child = child;
 	return t;
+}
+
+static inline CulTree *cul_tree_parent(CulTree *t) {
+	return t->parent;
+}
+
+static inline CulTree *cul_tree_child(CulTree *t) {
+	return t->child;
+}
+
+static inline CulTree *cul_tree_next(CulTree *t) {
+	return t->next;
+}
+
+static inline CulTree *cul_tree_prev(CulTree *t) {
+	return t->prev;
 }
 
 #endif
--- trunk/src/cul_tree.c
+++ trunk/src/cul_tree.c
@@ -22,6 +22,113 @@ void cul_tree_free(CulTree *t, cul_free_f *free_item) {
 }
 
 void cul_tree_free_all(CulTree *t, cul_free_f *free_item) {
-	CUL_UNUSED(t);
-	CUL_UNUSED(free_item);
+	for( CulTree *n; t != NULL; t = n) {
+		n = cul_tree_next(t);
+
+		/* free childs */
+		cul_tree_free_all(cul_tree_child(t), free_item);
+
+		/* free itself */
+		if( free_item != NULL )
+			free_item(t->data);
+		cul_tree_free_struct(t);
+	}
+}
+
+CulTree *cul_tree_first(CulTree *t) {
+	if( t != NULL )
+		while( cul_tree_prev(t) != NULL )
+			t = cul_tree_prev(t);
+	return t;
+}
+
+CulTree *cul_tree_last(CulTree *t) {
+	if( t != NULL )
+		while( cul_tree_next(t) != NULL )
+			t = cul_tree_next(t);
+	return t;
+}
+
+CulTree *cul_tree_root(CulTree *t) {
+	if( t != NULL )
+		while( cul_tree_parent(t) != NULL )
+			t = cul_tree_parent(t);
+	return t;
+}
+
+CulTree *cul_tree_insert_next(CulTree *t, cul_ptr data) {
+	CulTree *n;
+	if( (n = cul_tree_new_struct()) == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(NULL, CUL_ENOMEM);
+
+	if( t != NULL ) {
+		CulTree *next = cul_tree_next(t);
+
+		cul_tree_init_struct(n, t->parent, next, t, NULL, data);
+		/* adjust tree pointers */
+		if( next != NULL )
+			next->prev = n;
+		t->next = n;
+	}
+	else
+		cul_tree_init_struct(n, NULL, NULL, NULL, NULL, data);
+	return n;
+}
+
+CulTree *cul_tree_insert_prev(CulTree *t, cul_ptr data) {
+	CulTree *n;
+	if( (n = cul_tree_new_struct()) == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(NULL, CUL_ENOMEM);
+
+	if( t != NULL ) {
+		CulTree *prev = cul_tree_prev(t);
+
+		cul_tree_init_struct(n, t->parent, t, prev, NULL, data);
+		/* adjust tree pointers */
+		if( prev != NULL )
+			prev->next = n;
+		else
+			cul_tree_parent(t)->child = n;
+		t->prev = n;
+	}
+	else
+		cul_tree_init_struct(n, NULL, NULL, NULL, NULL, data);
+	return n;
+}
+
+CulTree *cul_tree_insert_child_first(CulTree *t, cul_ptr data) {
+	CulTree *n;
+	if( (n = cul_tree_new_struct()) == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(NULL, CUL_ENOMEM);
+
+	if( t != NULL ) {
+		CulTree *child = cul_tree_child(t);
+
+		cul_tree_init_struct(n, t, child, NULL, NULL, data);
+		/* adjust tree pointers */
+		if( child != NULL )
+			child->prev = n;
+		t->child = n;
+	}
+	else
+		cul_tree_init_struct(n, NULL, NULL, NULL, NULL, data);
+	return n;
+}
+
+CulTree *cul_tree_insert_child_last(CulTree *t, cul_ptr data) {
+	CulTree *n;
+	if( (n = cul_tree_new_struct()) == NULL )
+		CUL_ERROR_ERRNO_RET_VAL(NULL, CUL_ENOMEM);
+
+	if( t != NULL ) {
+		CulTree *child = cul_tree_last(cul_tree_child(t));
+
+		cul_tree_init_struct(n, t, NULL, child, NULL, data);
+		/* adjust tree pointers */
+		if( child != NULL )
+			child->next = n;
+	}
+	else
+		cul_tree_init_struct(n, NULL, NULL, NULL, NULL, data);
+	return n;
 }
