f3b27c657a13df0df2e95982ac21cdcf875bb34d
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-04-11 01:25:38 +0000

    * extend descriptions

--- trunk/include/cul/cul_arg.h
+++ trunk/include/cul/cul_arg.h
@@ -1,38 +1,53 @@
-#if !defined(CUL_ARG_H)
+#ifndef CUL_ARG_H
 #define CUL_ARG_H
 
 #include <cul/cul_global.h>
 
+typedef struct _CulArg CulArg;
 typedef enum _CulArgFlag CulArgFlag;
+
+/** \brief Argument flag describing its type and properties.
+ * 
+ * Argument flag describe how to interpret specific argument, if it take 
+ * additional option or is only switch. Argument could be marked as required.
+ * It is also possible to use specific argument entry only as additional 
+ * description.
+ * 
+ * NOTE: After argument parsing flag holds additional information, like if 
+ * argument was found on command line or its position.  
+ */ 
 enum _CulArgFlag {
 	/* type */
-	CUL_ARG_BOOL    = 1,  /* option is a switch, no additional parameter */
-	CUL_ARG_INT     = 2,  /* option has an integer number parameter (int) */
-	CUL_ARG_DOUBLE  = 3,  /* option has an floating point number parameter (double) */
-	CUL_ARG_STR     = 4,  /* option has an string parameter */
-	CUL_ARG_STRV    = 5,  /* option has a string array parameter, store all flag parameters */
+	CUL_ARG_BOOL    = 1,  /**< option is a switch, no additional parameter */
+	CUL_ARG_INT     = 2,  /**< option has an integer number parameter (int) */
+	CUL_ARG_DOUBLE  = 3,  /**< option has an floating point number parameter (double) */
+	CUL_ARG_STR     = 4,  /**< option has an string parameter */
+	CUL_ARG_STRV    = 5,  /**< option has a string array parameter, store all flag parameters */
 
 	/* flags */
-	CUL_ARG_END        = 1 << 4,  /* indicate last option in argument table */
-	CUL_ARG_JOIN       = 1 << 5,  /* indicate join of two tables */
-	CUL_ARG_FOUND      = 1 << 6,  /* option was found in application aguments */
-	CUL_ARG_HELP       = 1 << 7,  /* indicate no option, entry contains only help message to print on screen */
-	CUL_ARG_REQUIRED   = 1 << 8,  /* option is required */
+	CUL_ARG_END        = 1 << 4,  /**< indicate last option in argument table */
+	CUL_ARG_HELP       = 1 << 5,  /**< indicate no option, entry contains only help message to print on screen */
+	CUL_ARG_REQUIRED   = 1 << 6,  /**< option is required */
+	CUL_ARG_FOUND      = 1 << 7,  /**< option was found in application aguments */
 
 	/* masks */
-	CUL_ARG_TYPE_MASK  = 0x000f,       /* mask for argument type */
-	CUL_ARG_CONF_MASK  = 0xfff,        /* mask for argument configuration, flags and type */
-	CUL_ARG_POS_MASK   = 0xffff << 12, /* mask for argument position */
-	CUL_ARG_MASK       = 0xfffffff     /* maximal possible value for flag */
+	CUL_ARG_TYPE_MASK  = 0x000f,       /**< mask for argument type */
+	CUL_ARG_CONF_MASK  = 0xfff,        /**< mask for argument configuration, flags and type */
+	CUL_ARG_POS_MASK   = 0xffff << 12, /**< mask for argument position */
+	CUL_ARG_MASK       = 0xfffffff     /**< maximal possible flag value */
 };
 
-typedef struct _CulArg CulArg;
+/** \brief Argument description structure.
+ * 
+ * Structure describes single command line argument or additional 
+ * help/description that should be printed as a help information.
+ */
 struct _CulArg {
-	CulArgFlag flags;          /* option flags */
-	const char short_name;     /* short name for option */
-	const char *long_name;     /* long name for option */
-	const char *desciption;    /* description of option */
-	cul_ptr value;             /* callback for value, if option is not found it is left unchanged */
+	CulArgFlag flags;          /**< option flags */
+	const char short_name;     /**< short name for option */
+	const char *long_name;     /**< long name for option */
+	const char *desciption;    /**< description of option */
+	cul_ptr value;             /**< callback for value, if option is not found it is left unchanged */
 };
 
 cul_errno cul_arg_parse(int *argc, char ***argv, CulArg **table);
@@ -44,4 +59,4 @@ CulArg *cul_arg_check_required(CulArg *table);
 CulArg *cul_arg_search_short(CulArg *t, char arg);
 CulArg *cul_arg_search_long(CulArg *t, const char *arg);
 
-#endif
+#endif /* CUL_ARG_H */
--- trunk/include/cul/cul_string.h
+++ trunk/include/cul/cul_string.h
@@ -9,7 +9,8 @@
 
 typedef struct _CulString CulString;
 
-/**
+/** \brief CulString is manged string structure.
+ *  
  * CulString is similar to standard C string, except that it automatically
  * changes its size during various operations like text insert, cut or trim.
  * All operation are invatiant to nul bytes so it can be used to hold binary
@@ -21,14 +22,15 @@ struct _CulString {
 	size_t reserved; /**< actual reserved memory for given string */
 };
 
+/** \brief NULL CulString string.
+ * 
+ * CulStringNULL is a NULL CulString that could be used as initializer or 
+ * empty string argument.
+ */
 static const CulString CulStringNULL = {NULL, 0, 0};
-
-/* Basic Checks */
 
 static inline cul_bool cul_string_isnull(const CulString *s);
 static inline cul_bool cul_string_isempty(const CulString *s);
-
-/* Allocation */
 
 static inline CulString *cul_string_new_struct(void);
 static inline void cul_string_free_struct(CulString *s);
@@ -43,21 +45,15 @@ CulString *cul_string_new_size(size_t size);
 
 void cul_string_free(CulString *s);
 
-/* Resize */
-
 CulString *cul_string_clean(CulString *s);
 CulString *cul_string_clear(CulString *s);
 CulString *cul_string_resize(CulString *s, size_t size);
 CulString *cul_string_reserve(CulString *s, size_t size);
 CulString *cul_string_shrink(CulString *s);
 
-/* Copy */
-
 CulString *cul_string_copy(CulString *s, const CulString *copy);
 CulString *cul_string_copy_raw(CulString *s, const char *copy);
 CulString *cul_string_copy_raw_size(CulString *s, const char *copy, size_t size);
-
-/* Insert/Cut */
 
 CulString *cul_string_append(CulString *s, const CulString *append);
 CulString *cul_string_append_printf(CulString *s, const char *format, ...);
@@ -74,16 +70,12 @@ CulString *cul_string_cut(CulString *s, size_t x, size_t size);
 CulString *cul_string_cut_begin(CulString *s, size_t size);
 CulString *cul_string_cut_end(CulString *s, size_t size);
 
-/* Check */
-
 cul_bool cul_string_start_with(const CulString *s, const CulString *prefix);
 cul_bool cul_string_start_with_raw(const CulString *s, const char *prefix);
 cul_bool cul_string_end_with(const CulString *s, const CulString *postfix);
 cul_bool cul_string_end_with_raw(const CulString *s, const char *postfix);
 cul_bool cul_string_equal(const CulString *s, const CulString *compare);
 cul_bool cul_string_equal_raw(const CulString *s, const char *compare);
-
-/* Misc */
 
 CulString *cul_string_trim(CulString *s);
 CulString *cul_string_trim_left(CulString *s);
