16a4d678ad5077d470c10c8c348d35441b7922df
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-04-24 05:29:19 +0000

    * add testing capabilities with dummy inital tests

---
+++ trunk/tests/cul_base-test.c
@@ -0,0 +1,6 @@
+#include <cul/cul_global.h>
+#include <cul/cul_base.h>
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
---
+++ trunk/tests/cul_rng-test.c
@@ -0,0 +1,6 @@
+#include <cul/cul_global.h>
+#include <cul/cul_rng.h>
+
+int main(int argc, char* argv[]) {
+	return 1;
+}
