3f8b2b2e4cb8f6105ce3c53da8e438b327aaeb5e
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-05-01 23:10:00 +0000

    * remove unneeded wrappers

--- trunk/include/cul/cul_str_type.h
+++ trunk/include/cul/cul_str_type.h
@@ -1,4 +1,4 @@
-#if !defined(CUL_STR_TYPE_H)
+#ifndef CUL_STR_TYPE_H
 #define CUL_STR_TYPE_H
 
 #include <cul/cul_global.h>
@@ -24,63 +24,38 @@ enum _CulStringType {
 extern const uint16_t *const cul_str_type_ascii;
 
 static inline int cul_isspace(int c);
-static inline int cul_isspace_ascii(int c);
-static inline int cul_iswhite_ascii(int c);
+static inline int cul_iswhite(int c);
 
 static inline int cul_isupper(int c);
-static inline int cul_isupper_ascii(int c);
 static inline int cul_islower(int c);
-static inline int cul_islower_ascii(int c);
 
 static inline int cul_tolower(int c);
-static inline int cul_tolower_ascii(int c);
 static inline int cul_toupper(int c);
-static inline int cul_toupper_ascii(int c);
 
 /* implementations */
 
 static inline int cul_isspace(int c) {
-	return isspace(c);
-}
-
-static inline int cul_isspace_ascii(int c) {
 	return ( (cul_str_type_ascii[ (uint8_t)c ] & CUL_STRT_SPACE) != 0 );
 }
 
-static inline int cul_iswhite_ascii(int c) {
+static inline int cul_iswhite(int c) {
 	return ( (cul_str_type_ascii[ (uint8_t)c ] & CUL_STRT_WHITE) != 0 );
 }
 
 static inline int cul_isupper(int c) {
-	return isupper(c);
-}
-
-static inline int cul_isupper_ascii(int c) {
 	return ( (cul_str_type_ascii[ (uint8_t)c ] & CUL_STRT_UPPER) != 0 );
 }
 
 static inline int cul_islower(int c) {
-	return islower(c);
-}
-
-static inline int cul_islower_ascii(int c) {
 	return ( (cul_str_type_ascii[ (uint8_t)c ] & CUL_STRT_LOWER) != 0 );
 }
 
 static inline int cul_tolower(int c) {
-	return tolower(c);
-}
-
-static inline int cul_tolower_ascii(int c) {
-	return cul_islower_ascii(c)? c - 'a' + 'A': c;
+	return cul_islower(c)? c - 'a' + 'A': c;
 }
 
 static inline int cul_toupper(int c) {
-	return toupper(c);
+	return cul_isupper(c)? c - 'A' + 'a': c;
 }
 
-static inline int cul_toupper_ascii(int c) {
-	return cul_isupper_ascii(c)? c - 'A' + 'a': c;
-}
-
-#endif
+#endif /* CUL_STR_TYPE_H */
