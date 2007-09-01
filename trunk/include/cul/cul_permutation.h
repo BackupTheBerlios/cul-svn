432718f76b20a5e73e5422c6fca5985d0d936834
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-09-01 10:21:26 +0000

    * change define convention  * fix new indentation convention

--- trunk/include/cul/cul_permutation.h
+++ trunk/include/cul/cul_permutation.h
@@ -1,15 +1,15 @@
-#ifndef CUL_PERMUTATION_H
-#define CUL_PERMUTATION_H
+#ifndef __CUL_PERMUTATION_H__
+#define __CUL_PERMUTATION_H__
 
 #include <cul/cul_global.h>
 #include <cul/cul_uvector.h>
 #include <cul/cul_rng.h>
 
-cul_bool cul_permutation_valid(const CulUVector *p);
+cul_bool cul_permutation_valid   (const CulUVector *p);
 
-void cul_permutation_identity(CulUVector *p);
-void cul_permutation_shuffle(CulUVector *p, const CulRng *rng);
+void     cul_permutation_identity(CulUVector *p);
+void     cul_permutation_shuffle (CulUVector *p, const CulRng *rng);
 
-void cul_permutation_reverse(CulUVector *p);
+void     cul_permutation_reverse (CulUVector *p);
 
-#endif /*CUL_PERMUTATION_H*/
+#endif /* __CUL_PERMUTATION_H__ */
