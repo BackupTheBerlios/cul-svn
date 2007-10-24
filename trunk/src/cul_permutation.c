3e697808f5615fd2298b4e572017bf0f60698eb5
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2007-10-24 20:20:36 +0000

    * add permutation functions for CulUVectorView (header already present)

--- trunk/src/cul_permutation.c
+++ trunk/src/cul_permutation.c
@@ -1,49 +1,99 @@
 #include <cul/cul_permutation.h>
 #include <cul/cul_ubase.h>
 
-cul_bool cul_permutation_valid(const CulUVector *p) {
-	const size_t *restrict data = p->data;
+cul_bool cul_permutation_valid(const CulUVector *this) {
+	const size_t *restrict data = this->data;
 
-	const size_t size = p->size;
-	for( size_t i=0; i<size; ++i) {
+	const size_t size = this->size;
+	for(size_t i = 0; i < size; ++i) {
 		/* check for item value */
 		if( data[i] >= size )
 			return CUL_FALSE;
 
 		/* check for duplicates */
-		for(size_t j=0; j<i; ++j) if( data[j] == data[i] )
+		for(size_t j = 0; j < i; ++j) if( data[j] == data[i] )
 			return CUL_FALSE;
 	}
 	return CUL_TRUE;
 }
 
-void cul_permutation_identity(CulUVector *p) {
-	size_t *restrict data = p->data;
+cul_bool cul_permutationview_valid(const CulUVectorView *this) {
+	const size_t *restrict data = this->data;
 
-	const size_t size = p->size;
-	for( size_t i=0; i<size; ++i)
+	const size_t stride = this->stride;
+	const size_t size = this->size;
+	for(size_t i = 0; i < size; i += stride) {
+		/* check for item value */
+		if( data[i] >= size )
+			return CUL_FALSE;
+
+		/* check for duplicates */
+		for(size_t j = 0; j < i; j += stride) if( data[j] == data[i] )
+			return CUL_FALSE;
+	}
+	return CUL_TRUE;
+}
+
+
+void cul_permutation_identity(CulUVector *this) {
+	size_t *restrict data = this->data;
+
+	const size_t size = this->size;
+	for(size_t i = 0; i < size; ++i)
 		data[i] = i;
 }
 
-void cul_permutation_shuffle(CulUVector *p, const CulRng *rng) {
-	size_t *restrict data = p->data;
+void cul_permutation_shuffle(CulUVector *this, const CulRng *rng) {
+	size_t *restrict data = this->data;
 	size_t tmp, pos;
 
-	for(size_t i = p->size - 1; i > 0; --i) {
+	for(size_t i = this->size - 1; i > 0; --i) {
 		pos = cul_rng_get_real(rng) * (i+1);
 		CUL_SWAP(data[i], data[pos], tmp);
 	}
 }
 
-void cul_permutation_reverse(CulUVector *p) {
-	size_t *restrict data = p->data;
+void cul_permutation_reverse(CulUVector *this) {
+	size_t *restrict data = this->data;
 	size_t tmp, pos;
 
-	const size_t size = p->size;
-	const size_t half = size >> 1;
-
+	const size_t half = this->size >> 1;
+	const size_t size = this->size - 1;
 	for(size_t i = 0; i < half; ++i) {
-		pos = size - i - 1;
+		pos = size - i;
+		CUL_SWAP(data[i], data[pos], tmp);
+	}
+}
+
+void cul_permutationview_identity(CulUVectorView *this) {
+	size_t *restrict data = this->data;
+
+	const size_t stride = this->stride;
+	const size_t size = this->size * stride;
+	for(size_t i = 0, j = 0; i < size; i += stride, ++j)
+		data[i] = j;
+}
+
+void cul_permutationview_shuffle (CulUVectorView *this, const CulRng *rng) {
+	size_t *restrict data = this->data;
+	size_t tmp, pos;
+
+	const size_t stride = this->stride;
+	for(size_t i = (this->size - 1) * stride, j = this->size; i > 0; i -= stride, --j) {
+		pos = cul_rng_get_real(rng) * j;
+		CUL_SWAP(data[i], data[pos * stride], tmp);
+	}
+}
+
+void cul_permutationview_reverse (CulUVectorView *this) {
+	size_t *restrict data = this->data;
+	size_t tmp, pos;
+
+	const size_t stride = this->stride;
+	const size_t half = (this->size >> 1) * stride;
+	const size_t size = (this->size - 1) * stride;
+	for(size_t i = 0; i < half; i += stride) {
+		pos = size - i;
 		CUL_SWAP(data[i], data[pos], tmp);
 	}
 }
