deaf8b99f2207f08618f991fabf5ff8d29b8088d
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2008-11-26 10:42:32 +0000

    * fix template headers names

--- trunk/src/private/cul_copy_prv.c
+++ trunk/src/private/cul_copy_prv.c
@@ -1,33 +1,33 @@
 #include <cul/cul_global.h>
-#include <cul_prv_copy.h>
+#include "cul_copy_prv.h"
 
-#define TEMPLATE_CUL_CHAR
+#define TEMPLATE_CHAR
 #include "cul_templates_on.h"
-#include "cul_prv_copy-template.c"
+#include "cul_copy_prv-template.c"
 #include "cul_templates_off.h"
-#undef  TEMPLATE_CUL_CHAR
+#undef  TEMPLATE_CHAR
 
-#define TEMPLATE_CUL_DOUBLE
+#define TEMPLATE_DOUBLE
 #include "cul_templates_on.h"
-#include "cul_prv_copy-template.c"
+#include "cul_copy_prv-template.c"
 #include "cul_templates_off.h"
-#undef  TEMPLATE_CUL_DOUBLE
+#undef  TEMPLATE_DOUBLE
 
-#define TEMPLATE_CUL_INT
+#define TEMPLATE_INT
 #include "cul_templates_on.h"
-#include "cul_prv_copy-template.c"
+#include "cul_copy_prv-template.c"
 #include "cul_templates_off.h"
-#undef  TEMPLATE_CUL_INT
+#undef  TEMPLATE_INT
 
-#define TEMPLATE_CUL_SIZE
+#define TEMPLATE_SIZE_T
 #include "cul_templates_on.h"
-#include "cul_prv_copy-template.c"
+#include "cul_copy_prv-template.c"
 #include "cul_templates_off.h"
-#undef  TEMPLATE_CUL_SIZE
+#undef  TEMPLATE_SIZE_T
 
 #define TEMPLATE_CUL_PTR
 #include "cul_templates_on.h"
-#include "cul_prv_copy-template.c"
+#include "cul_copy_prv-template.c"
 #include "cul_templates_off.h"
 #undef  TEMPLATE_CUL_PTR
 
