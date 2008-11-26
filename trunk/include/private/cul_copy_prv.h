676c56050a77fd022449bbbe3724288aa77bb51b
moloh <moloh@29d5f6da-c227-0410-85a8-f319873d57ae>
2008-11-26 10:41:09 +0000

    * migrate to header templates engine

---
+++ trunk/include/private/cul_copy_prv.h
@@ -0,0 +1,11 @@
+#ifndef __CUL_COPY_PRV_H__
+#define __CUL_COPY_PRV_H__
+
+#include <cul_ccopy_prv.h>
+#include <cul_dcopy_prv.h>
+#include <cul_icopy_prv.h>
+#include <cul_pcopy_prv.h>
+#include <cul_ucopy_prv.h>
+
+#endif /* __CUL_COPY_PRV_H__ */
+
---
+++ trunk/include/private/cul_sort_prv.h
@@ -0,0 +1,11 @@
+#ifndef __CUL_SORT_PRV_H__
+#define __CUL_SORT_PRV_H__
+
+#include <cul_csort_prv.h>
+#include <cul_dsort_prv.h>
+#include <cul_isort_prv.h>
+#include <cul_psort_prv.h>
+#include <cul_usort_prv.h>
+
+#endif /* __CUL_SORT_PRV_H__ */
+
